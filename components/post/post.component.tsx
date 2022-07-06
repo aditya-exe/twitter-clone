df5641eaa5602a4bec087a26aa7d63bda1a9b844
aditya <pokemaniacaditya@gmail.com>
2022-07-07 00:38:57 +0530

    added like feature

--- components/post/post.component.tsx
+++ components/post/post.component.tsx
@@ -1,7 +1,13 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import { ChartBarIcon, ChatIcon, DotsHorizontalIcon, HeartIcon, ShareIcon, TrashIcon } from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import { collection, deleteDoc, doc, onSnapshot, setDoc } from "firebase/firestore";
 import Moment from "react-moment";
+import { db } from "../../firebase";
+import { signIn, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
 
 // interface PostProps {
 //   post: {
@@ -16,6 +22,35 @@ import Moment from "react-moment";
 // };
 
 const Post: React.FC<any> = ({ post }) => {
+  const { data: session } = useSession();
+  const [likes, setLikes] = useState<any>([]);
+  const [hasLiked, setHasLikes] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "posts", post.id, "likes"),
+      (snapshot) => setLikes(snapshot.docs)
+    )
+  }, [db])
+
+  useEffect(() => {
+    setHasLikes(likes.findIndex((like: { id: string | undefined; }) => like.id === session?.user.uid) !== -1)
+  }, [likes])
+
+  const likePost = async () => {
+    if (session) {
+      if (hasLiked) {
+        await deleteDoc(doc(db, "posts", post.id, "likes", session?.user.uid))
+      } else {
+        await setDoc(doc(db, "posts", post.id, "likes", session?.user.uid), {
+          username: session?.user.username,
+        })
+      }
+    }else{
+      signIn();
+    }
+  }
+
   return (
     <div className="flex p-3 cursor-pointer border-b border-gray-200">
       <img src={post.data().userImg} alt={"user-img"} className="h-11 w-11 rounded-full mr-4" />
@@ -26,7 +61,7 @@ const Post: React.FC<any> = ({ post }) => {
             <span className="text-sm sm:text-[15px]">@{post.data().username} - </span>
             <span className="text-sm sm:text-[15px] hover:underline">
               <Moment fromNow>
-                {post?.timestamp?.toDate()}
+                {post?.data().timestamp?.toDate()}
               </Moment>
             </span>
           </div>
@@ -37,7 +72,16 @@ const Post: React.FC<any> = ({ post }) => {
         <div className="flex justify-between text-gray-500">
           <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
           <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
-          <HeartIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+          <div className="flex items-center">
+            {hasLiked ? (
+              <HeartIconFilled onClick={likePost} className="h-9 w-9 hoverEffect p-2 text-red-600 hover:bg-red-100" />
+            ) : (
+              <HeartIcon onClick={likePost} className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+            )}
+            {likes.length > 0 && (
+              <span className={`${hasLiked && "text-red-600"} text-sm select-none`}>{likes.length}</span>
+            )}
+          </div>
           <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
           <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
         </div>
