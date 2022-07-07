619232f41ddb1a83d3eacc3ca7535c469c304adf
aditya <pokemaniacaditya@gmail.com>
2022-07-07 23:56:54 +0530

    added comments feature

--- components/comment-modal/comment-modal.component.tsx
+++ components/comment-modal/comment-modal.component.tsx
@@ -1,13 +1,103 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import { useRecoilState } from "recoil";
-import { modalState } from "../../atom/modal.atom";
+import { modalState, postIdState } from "../../atom/modal.atom";
+import Modal from "react-modal";
+import { XIcon } from "@heroicons/react/solid";
+import { useEffect, useRef, useState } from "react";
+import { db } from "../../firebase";
+import { addDoc, collection, doc, onSnapshot, serverTimestamp } from "firebase/firestore";
+import Moment from "react-moment";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { EmojiHappyIcon, PhotographIcon } from "@heroicons/react/outline";
 
 const CommentModal = () => {
   const [open, setOpen] = useRecoilState(modalState);
+  const [postId] = useRecoilState(postIdState);
+  const [post, setPost] = useState<any>();
+  const { data: session } = useSession();
+  const [input, setInput] = useState<string>("");
+  const router = useRouter();
+  const filePickerRef=useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    onSnapshot(doc(db, "posts", postId),
+      (snapshot) => {
+        setPost(snapshot);
+      });
+  }, [postId]);
+
+  const sendComment = async () => {
+    await addDoc(collection(db, "posts", postId, "comments"), {
+      comment: postId,
+      name: session?.user.name,
+      username: session?.user.username,
+      userImg: session?.user.image,
+      timestamp: serverTimestamp(),
+    });
+
+    setOpen(false);
+    setInput("");
+    router.push(`/posts/${postId}`);
+  };
 
   return (
     <div>
-      <h1>Comment Modal</h1>
-      {open && <h1>Open</h1>}
+      {open && (
+        <Modal
+          isOpen={open}
+          onRequestClose={() => setOpen(false)}
+          className="max-w-lg w-[90%] absolute top-24 left-[50%] translate-x-[-50%] bg-white border-2 border-gray-200 rounded-xl shadow-md h-[300px]"
+        >
+          <div className="p-1">
+            <div className="border-b border-gray-200 py-2 px-1.5 ">
+              <div className="hoverEffect w-9 h-9 flex items-center justify-center">
+                <XIcon onClick={() => setOpen(false)} className="h-[22px] text-gray-700" />
+              </div>
+            </div>
+            <div className="p-2 flex items-center space-x-1 relative">
+              <span className="w-0.5 h-full z-[-1] absolute left-8 top-11 bg-gray-300" />
+              <img src={post?.data()?.userImg} alt={"user-img"} className="h-11 w-11 rounded-full mr-4" />
+              <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post?.data()?.name}</h4>
+              <span className="text-sm sm:text-[15px]">@{post?.data()?.username} - </span>
+              <span className="text-sm sm:text-[15px] hover:underline">
+                <Moment fromNow>
+                  {post?.data()?.timestamp?.toDate()}
+                </Moment>
+              </span>
+            </div>
+            <p className="text-gray-500 text-[15px] sm:text-[16px] ml-16 mb-2 " >{post?.data()?.text}</p>
+            <div className="flex border-b border-gray-200 p-3 space-x-3">
+              <img
+                src={session?.user?.image}
+                className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"
+              />
+              <div className="w-full divide-y divide-gray-200 ">
+                <div className="">
+                  <textarea
+                    className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700 "
+                    rows={2}
+                    placeholder="What's happening?"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                  ></textarea>
+                </div>
+                <div className="flex items-center justify-between pt-2.5 ">
+                  <div className="flex">
+                    <div onClick={() => { if (filePickerRef !== null) filePickerRef.current?.click() }}>
+                      <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+                      <input type="file" hidden ref={filePickerRef} />
+                    </div>
+                    <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+                  </div>
+                  <button onClick={sendComment} disabled={!input.trim()} className="bg-blue-400 text-white cursor-pointer px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">Reply</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      )}
     </div>
   )
 }
--- components/post/post.component.tsx
+++ components/post/post.component.tsx
@@ -9,7 +9,8 @@ import { signIn, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { deleteObject, ref } from "firebase/storage";
 import { useRecoilState } from "recoil";
-import { modalState } from "../../atom/modal.atom";
+import { modalState, postIdState } from "../../atom/modal.atom";
+import { useRouter } from "next/router";
 
 
 // interface PostProps {
@@ -24,29 +25,39 @@ import { modalState } from "../../atom/modal.atom";
 //   }
 // };
 
-const Post: React.FC<any> = ({ post }) => {
+const Post: React.FC<any> = ({ post, id }) => {
   const { data: session } = useSession();
   const [likes, setLikes] = useState<any>([]);
   const [hasLiked, setHasLikes] = useState(false);
-  const [open, setOpen] = useRecoilState(modalState)
+  const [comments, setComments] = useState<any>([]);
+  const [open, setOpen] = useRecoilState(modalState);
+  const [postId, setPostId] = useRecoilState(postIdState);
+  const router = useRouter();
 
   useEffect(() => {
     const unsubscribe = onSnapshot(
-      collection(db, "posts", post.id, "likes"),
+      collection(db, "posts", id, "likes"),
       (snapshot) => setLikes(snapshot.docs)
     )
-  }, [db])
+  }, [id])
+
+  useEffect(() => {
+    const unsubsribe = onSnapshot(
+      collection(db, "posts", id, "comments"),
+      (snapshot) => setComments(snapshot.docs)
+    )
+  }, [id])
 
   useEffect(() => {
     setHasLikes(likes.findIndex((like: { id: string | undefined; }) => like.id === session?.user.uid) !== -1)
-  }, [likes])
+  }, [likes, session?.user.uid])
 
   const likePost = async () => {
     if (session) {
       if (hasLiked) {
-        await deleteDoc(doc(db, "posts", post.id, "likes", session?.user.uid))
+        await deleteDoc(doc(db, "posts", id, "likes", session?.user.uid))
       } else {
-        await setDoc(doc(db, "posts", post.id, "likes", session?.user.uid), {
+        await setDoc(doc(db, "posts", id, "likes", session?.user.uid), {
           username: session?.user.username,
         })
       }
@@ -57,23 +68,22 @@ const Post: React.FC<any> = ({ post }) => {
 
   const deletePost = async () => {
     if (window.confirm("Are you sure you want to delete this post?")) {
-      deleteDoc(doc(db, "posts", post.id));
+      deleteDoc(doc(db, "posts", id));
       if (post.data().image) {
-        deleteObject(ref(storage, `posts/${post.id}/image`));
+        deleteObject(ref(storage, `posts/${id}/image`));
       }
+      router.push("/");
     }
   }
 
-
-
   return (
     <div className="flex p-3 cursor-pointer border-b border-gray-200">
-      <img src={post.data().userImg} alt={"user-img"} className="h-11 w-11 rounded-full mr-4" />
-      <div>
+      <img src={post?.data()?.userImg} alt={"user-img"} className="h-11 w-11 rounded-full mr-4" />
+      <div className="flex-1">
         <div className="flex items-center justify-between ">
           <div className="flex space-x-1 whitespace-nowrap items-center">
-            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post.data().name}</h4>
-            <span className="text-sm sm:text-[15px]">@{post.data().username} - </span>
+            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post?.data().name}</h4>
+            <span className="text-sm sm:text-[15px]">@{post?.data().username} - </span>
             <span className="text-sm sm:text-[15px] hover:underline">
               <Moment fromNow>
                 {post?.data().timestamp?.toDate()}
@@ -82,10 +92,25 @@ const Post: React.FC<any> = ({ post }) => {
           </div>
           <DotsHorizontalIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2" />
         </div>
-        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2 ">{post.data().text}</p>
-        <img className="rounded-2xl mr-2" src={post.data().image} />
+        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2 ">{post?.data().text}</p>
+        <img className="rounded-2xl mr-2" src={post?.data().image} />
         <div className="flex justify-between text-gray-500">
-          <ChatIcon onClick={() => setOpen} className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <div className="flex items-center select-none">
+            <ChatIcon onClick={() => {
+              if (!session) {
+                signIn();
+              } else {
+                setPostId(id);
+                setOpen(!open);
+              }
+            }}
+              className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+            {comments.length > 0 && (
+              <span className="text-sm">
+                {comments.length}
+              </span>
+            )}
+          </div>
           {session?.user.uid === post?.data().id && (
             <TrashIcon onClick={deletePost} className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
           )}
